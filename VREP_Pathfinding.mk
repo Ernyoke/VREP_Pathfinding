1b5dc9fb44060f11848b87127e272ca621080b9c
ervin_acc_virtualbox <ervin509@yahoo.com>
2016-05-04 11:24:24 +0300

    Debug and Release folders added ot .gitignore

--- VREP_Pathfinding.mk
+++ VREP_Pathfinding.mk
@@ -6,14 +6,14 @@
 ProjectName            :=VREP_Pathfinding
 ConfigurationName      :=Debug
 WorkspacePath          :=/home/ervin/CProjects/codelite_workspace/codelite_workspace
-ProjectPath            :=/home/ervin/CProjects/codelite_workspace/VREP_Pathfinding
+ProjectPath            :=/home/ervin/CProjects/codelite_workspace/GIT/VREP_Pathfinding
 IntermediateDirectory  :=./Debug
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
 User                   :=ervin
-Date                   :=03/05/16
+Date                   :=04/05/16
 CodeLitePath           :=/home/ervin/.codelite
 LinkerName             :=/usr/bin/g++
 SharedObjectLinkerName :=/usr/bin/g++ -shared -fPIC
@@ -93,7 +93,7 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/VREP_Pathfinding/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/GIT/VREP_Pathfinding/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM "main.cpp"
 
@@ -101,7 +101,7 @@ $(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) "main.cpp"
 
 $(IntermediateDirectory)/Playground.cpp$(ObjectSuffix): Playground.cpp $(IntermediateDirectory)/Playground.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/VREP_Pathfinding/Playground.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Playground.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/GIT/VREP_Pathfinding/Playground.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Playground.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Playground.cpp$(DependSuffix): Playground.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Playground.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Playground.cpp$(DependSuffix) -MM "Playground.cpp"
 
@@ -109,7 +109,7 @@ $(IntermediateDirectory)/Playground.cpp$(PreprocessSuffix): Playground.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Playground.cpp$(PreprocessSuffix) "Playground.cpp"
 
 $(IntermediateDirectory)/PathInterface.cpp$(ObjectSuffix): PathInterface.cpp $(IntermediateDirectory)/PathInterface.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/VREP_Pathfinding/PathInterface.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/PathInterface.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/GIT/VREP_Pathfinding/PathInterface.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/PathInterface.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/PathInterface.cpp$(DependSuffix): PathInterface.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/PathInterface.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/PathInterface.cpp$(DependSuffix) -MM "PathInterface.cpp"
 
@@ -117,7 +117,7 @@ $(IntermediateDirectory)/PathInterface.cpp$(PreprocessSuffix): PathInterface.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/PathInterface.cpp$(PreprocessSuffix) "PathInterface.cpp"
 
 $(IntermediateDirectory)/A_StarPath.cpp$(ObjectSuffix): A_StarPath.cpp $(IntermediateDirectory)/A_StarPath.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/VREP_Pathfinding/A_StarPath.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/A_StarPath.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/GIT/VREP_Pathfinding/A_StarPath.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/A_StarPath.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/A_StarPath.cpp$(DependSuffix): A_StarPath.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/A_StarPath.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/A_StarPath.cpp$(DependSuffix) -MM "A_StarPath.cpp"
 
@@ -125,7 +125,7 @@ $(IntermediateDirectory)/A_StarPath.cpp$(PreprocessSuffix): A_StarPath.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/A_StarPath.cpp$(PreprocessSuffix) "A_StarPath.cpp"
 
 $(IntermediateDirectory)/Position.cpp$(ObjectSuffix): Position.cpp $(IntermediateDirectory)/Position.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/VREP_Pathfinding/Position.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Position.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/GIT/VREP_Pathfinding/Position.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Position.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Position.cpp$(DependSuffix): Position.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Position.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Position.cpp$(DependSuffix) -MM "Position.cpp"
 
@@ -133,7 +133,7 @@ $(IntermediateDirectory)/Position.cpp$(PreprocessSuffix): Position.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Position.cpp$(PreprocessSuffix) "Position.cpp"
 
 $(IntermediateDirectory)/OutOfBoundException.cpp$(ObjectSuffix): OutOfBoundException.cpp $(IntermediateDirectory)/OutOfBoundException.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/VREP_Pathfinding/OutOfBoundException.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/OutOfBoundException.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/GIT/VREP_Pathfinding/OutOfBoundException.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/OutOfBoundException.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/OutOfBoundException.cpp$(DependSuffix): OutOfBoundException.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/OutOfBoundException.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/OutOfBoundException.cpp$(DependSuffix) -MM "OutOfBoundException.cpp"
 
@@ -141,7 +141,7 @@ $(IntermediateDirectory)/OutOfBoundException.cpp$(PreprocessSuffix): OutOfBoundE
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/OutOfBoundException.cpp$(PreprocessSuffix) "OutOfBoundException.cpp"
 
 $(IntermediateDirectory)/HistoricalPosition.cpp$(ObjectSuffix): HistoricalPosition.cpp $(IntermediateDirectory)/HistoricalPosition.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/VREP_Pathfinding/HistoricalPosition.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/HistoricalPosition.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/GIT/VREP_Pathfinding/HistoricalPosition.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/HistoricalPosition.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/HistoricalPosition.cpp$(DependSuffix): HistoricalPosition.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/HistoricalPosition.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/HistoricalPosition.cpp$(DependSuffix) -MM "HistoricalPosition.cpp"
 
@@ -149,7 +149,7 @@ $(IntermediateDirectory)/HistoricalPosition.cpp$(PreprocessSuffix): HistoricalPo
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/HistoricalPosition.cpp$(PreprocessSuffix) "HistoricalPosition.cpp"
 
 $(IntermediateDirectory)/NoPathException.cpp$(ObjectSuffix): NoPathException.cpp $(IntermediateDirectory)/NoPathException.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/VREP_Pathfinding/NoPathException.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/NoPathException.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/GIT/VREP_Pathfinding/NoPathException.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/NoPathException.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/NoPathException.cpp$(DependSuffix): NoPathException.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/NoPathException.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/NoPathException.cpp$(DependSuffix) -MM "NoPathException.cpp"
 
@@ -157,7 +157,7 @@ $(IntermediateDirectory)/NoPathException.cpp$(PreprocessSuffix): NoPathException
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/NoPathException.cpp$(PreprocessSuffix) "NoPathException.cpp"
 
 $(IntermediateDirectory)/VRepApi.cpp$(ObjectSuffix): VRepApi.cpp $(IntermediateDirectory)/VRepApi.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/VREP_Pathfinding/VRepApi.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/VRepApi.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/GIT/VREP_Pathfinding/VRepApi.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/VRepApi.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/VRepApi.cpp$(DependSuffix): VRepApi.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/VRepApi.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/VRepApi.cpp$(DependSuffix) -MM "VRepApi.cpp"
 
@@ -165,7 +165,7 @@ $(IntermediateDirectory)/VRepApi.cpp$(PreprocessSuffix): VRepApi.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/VRepApi.cpp$(PreprocessSuffix) "VRepApi.cpp"
 
 $(IntermediateDirectory)/remoteApi_extApi.c$(ObjectSuffix): remoteApi/extApi.c $(IntermediateDirectory)/remoteApi_extApi.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/VREP_Pathfinding/remoteApi/extApi.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/remoteApi_extApi.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/GIT/VREP_Pathfinding/remoteApi/extApi.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/remoteApi_extApi.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/remoteApi_extApi.c$(DependSuffix): remoteApi/extApi.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/remoteApi_extApi.c$(ObjectSuffix) -MF$(IntermediateDirectory)/remoteApi_extApi.c$(DependSuffix) -MM "remoteApi/extApi.c"
 
@@ -173,7 +173,7 @@ $(IntermediateDirectory)/remoteApi_extApi.c$(PreprocessSuffix): remoteApi/extApi
 	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/remoteApi_extApi.c$(PreprocessSuffix) "remoteApi/extApi.c"
 
 $(IntermediateDirectory)/remoteApi_extApiPlatform.c$(ObjectSuffix): remoteApi/extApiPlatform.c $(IntermediateDirectory)/remoteApi_extApiPlatform.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/VREP_Pathfinding/remoteApi/extApiPlatform.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/remoteApi_extApiPlatform.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "/home/ervin/CProjects/codelite_workspace/GIT/VREP_Pathfinding/remoteApi/extApiPlatform.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/remoteApi_extApiPlatform.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/remoteApi_extApiPlatform.c$(DependSuffix): remoteApi/extApiPlatform.c
 	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/remoteApi_extApiPlatform.c$(ObjectSuffix) -MF$(IntermediateDirectory)/remoteApi_extApiPlatform.c$(DependSuffix) -MM "remoteApi/extApiPlatform.c"
 
